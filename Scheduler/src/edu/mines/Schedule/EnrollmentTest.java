c9b0788fe5df4df4dc7962e04050aa40f651d1ed
Chris Loew <cloew@mymail.mines.edu>
2011-02-17 16:45:51 -0700

    Adding EnrollmentTest

---
+++ Scheduler/src/edu/mines/Schedule/EnrollmentTest.java
@@ -0,0 +1,16 @@
+package edu.mines.Schedule;
+
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+public class EnrollmentTest extends TestCase {
+	public Enrollment createEnrollment() {
+		student=
+	}
+
+	public void testCreateEnrollment() {
+		fail("Not yet implemented");
+	}
+
+}
--- Scheduler/src/edu/mines/Schedule/Student.java
+++ Scheduler/src/edu/mines/Schedule/Student.java
@@ -3,18 +3,18 @@ package edu.mines.Schedule;
 import java.util.ArrayList;
 
 public class Student extends Person {
-	public Student(String name, String cwid, ArrayList<Majors> majors) {
+	public Student(String name, String cwid, ArrayList<Major> majors) {
 		super(name, cwid);
 		this.majors = majors;
 	}
 
-	private ArrayList<Majors> majors;
+	private ArrayList<Major> majors;
 
-	public ArrayList<Majors> getMajor() {
+	public ArrayList<Major> getMajor() {
 		return majors;
 	}
 
-	public void setMajor(ArrayList<Majors> majors) {
+	public void setMajor(ArrayList<Major> majors) {
 		this.majors = majors;
 	}
 	
