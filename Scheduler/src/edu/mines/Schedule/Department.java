947c202abc7d1e1769102bf127b10b72c544e573
Chris Loew <cloew@mymail.mines.edu>
2011-02-17 16:22:48 -0700

    Added Department and DepartmentTest

---
+++ Scheduler/src/edu/mines/Schedule/Department.java
@@ -0,0 +1,35 @@
+package edu.mines.Schedule;
+
+import java.util.ArrayList;
+
+public class Department {
+	private final String name;
+	private final String code;
+	private ArrayList<Major> majors;
+	
+	Department(String name, String code) {
+		this.name = name;
+		this.code = code;
+		this.majors = new ArrayList<Major>();
+	}
+	
+	ArrayList<Major> getMajors() {
+		return (ArrayList<Major>) majors.clone();
+	}
+	
+	String getName() {
+		return name;
+	}
+	
+	String getCode() {
+		return code;
+	}
+	
+	boolean addMajor(Major major) {
+		return majors.add(major);
+	}
+	
+	boolean removeMajor(Major major) {
+		return false;
+	}
+}
---
+++ Scheduler/src/edu/mines/Schedule/DepartmentTest.java
@@ -0,0 +1,19 @@
+package edu.mines.Schedule;
+
+import junit.framework.TestCase;
+
+public class DepartmentTest extends TestCase {
+
+	public void testAddMajor() {
+		Department department = new Department("Computer Science", "CSCI");
+		assertEquals("Majors should be empty", department.getMajors().size(), 0);
+		Major major = Major.CompSci;
+		department.addMajor(major);
+		assertEquals("Majors should be empty", department.getMajors().contains(major), true);
+	}
+
+	public void testRemoveMajor() {
+		fail("Not yet implemented");
+	}
+
+}
