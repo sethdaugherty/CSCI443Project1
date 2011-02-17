d60932b52d683eb47d4360648d97b55b7befd0a8
Seth Daugherty <sdaugher@mines.edu>
2011-02-17 16:11:14 -0700

    Created meeting class

---
+++ Scheduler/src/edu/mines/Schedule/CourseMeeting.java
@@ -0,0 +1,51 @@
+package edu.mines.Schedule;
+
+import java.util.Date;
+
+public class CourseMeeting {
+	private Course course;
+	private Classroom classroom;
+	private Date meetingTime;
+	private int meetingLength;
+	
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
+	public Classroom getClassroom() {
+		return classroom;
+	}
+
+	public void setClassroom(Classroom classroom) {
+		this.classroom = classroom;
+	}
+
+	public Date getMeetingTime() {
+		return meetingTime;
+	}
+
+	public void setMeetingTime(Date time) {
+		this.meetingTime = time;
+	}
+
+	public int getMeetingLength() {
+		return meetingLength;
+	}
+
+	public void setMeetingLength(int length) {
+		this.meetingLength = length;
+	}
+
+	public CourseMeeting(Course course, Classroom room, Date time, int length) {
+		this.course = course;
+		this.classroom = room;
+		this.meetingTime = time;
+		this.meetingLength = length;
+	}
+
+
+}
