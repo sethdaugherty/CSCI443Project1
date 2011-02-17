baaf9c8e3beaaeb797cc4264f741beeb127f0704
Seth Daugherty <sdaugher@mines.edu>
2011-02-17 15:51:01 -0700

    Added skeleton person classes

---
+++ Scheduler/src/edu/mines/Schedule/Student.java
@@ -0,0 +1,14 @@
+package edu.mines.Schedule;
+
+public class Student extends Person {
+	private Majors major;
+
+	public Majors getMajor() {
+		return major;
+	}
+
+	public void setMajor(Majors major) {
+		this.major = major;
+	}
+	
+}
