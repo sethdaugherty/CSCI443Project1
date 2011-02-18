abf27669871003ae61e88a230d45916d79c4ba11
Chris Loew <cloew@mymail.mines.edu>
2011-02-17 17:02:39 -0700

    Added StudentTest

--- Scheduler/src/edu/mines/Schedule/EnrollmentTest.java
+++ Scheduler/src/edu/mines/Schedule/EnrollmentTest.java
@@ -1,12 +1,17 @@
 package edu.mines.Schedule;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 import junit.framework.TestCase;
 
 public class EnrollmentTest extends TestCase {
-	public Enrollment createEnrollment() {
-		student=
+	public static Enrollment createEnrollment() {
+		ArrayList<Major> majors = new ArrayList<Major>();
+		majors.add(Major.CompSci);
+		Student student = new Student("Bob Joe", "########", majors);
+		CourseMeeting courseMeeting = CourseMeetingTest.createMeeting();
+		return new Enrollment(student, courseMeeting);
 	}
 
 	public void testCreateEnrollment() {
